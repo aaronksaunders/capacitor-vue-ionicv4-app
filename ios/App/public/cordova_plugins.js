79f60884cdd769f9c5b90dba4797306a6e1fa162
aaron k saunders <aaronksaunders@gmail.com>
2019-08-26 21:30:06 -0400

    added scanner plugin

---
+++ ios/App/public/cordova_plugins.js
@@ -0,0 +1,20 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+          "cordova.plugins.barcodeScanner"
+        ]
+      }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "phonegap-plugin-barcodescanner": "8.1.0"
+    };
+    // BOTTOM OF METADATA
+    });
+    
--- src/components/CameraPage.vue
+++ src/components/CameraPage.vue
@@ -7,12 +7,9 @@
     </ion-header>
     <ion-content padding>
       <img :src="imageUrl ? imageUrl : null" />
-      <ion-button @click="takePicture()">
-        Take Picture Now
-      </ion-button>
-      <ion-button @click="nextPage()">
-        Next Page
-      </ion-button>
+      <ion-button @click="takePicture()">Take Picture Now</ion-button>
+      <ion-button @click="nextPage()">Next Page</ion-button>
+      <ion-button @click="scan()">SCAN</ion-button>
     </ion-content>
   </div>
 </template>
@@ -35,11 +32,43 @@ export default {
     };
   },
   methods: {
+    scan() {
+      cordova.plugins.barcodeScanner.scan(
+        function(result) {
+          alert(
+            "We got a barcode\n" +
+              "Result: " +
+              result.text +
+              "\n" +
+              "Format: " +
+              result.format +
+              "\n" +
+              "Cancelled: " +
+              result.cancelled
+          );
+        },
+        function(error) {
+          alert("Scanning failed: " + error);
+        },
+        {
+          preferFrontCamera: true, // iOS and Android
+          showFlipCameraButton: true, // iOS and Android
+          showTorchButton: true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt: "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations: true, // iOS
+          disableSuccessBeep: false // iOS and Android
+        }
+      );
+    },
     nextPage() {
       this.$router.push("/geo-location-page");
     },
     async takePicture() {
-
       let isAvailable = true;
 
       if (!isAvailable) {
