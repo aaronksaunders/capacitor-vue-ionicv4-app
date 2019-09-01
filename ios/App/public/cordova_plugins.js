71bdf2b50335a7a87bc43decd899ef5e35c4b0f0
aaron k saunders <aaronksaunders@gmail.com>
2019-09-01 01:18:24 -0400

    testing on android

--- ios/App/public/cordova_plugins.js
+++ ios/App/public/cordova_plugins.js
@@ -2,13 +2,13 @@
   cordova.define('cordova/plugin_list', function(require, exports, module) {
     module.exports = [
       {
-        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
-        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
-        "pluginId": "phonegap-plugin-barcodescanner",
+          "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+          "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+          "pluginId": "phonegap-plugin-barcodescanner",
         "clobbers": [
           "cordova.plugins.barcodeScanner"
         ]
-      }
+        }
     ];
     module.exports.metadata =
     // TOP OF METADATA
