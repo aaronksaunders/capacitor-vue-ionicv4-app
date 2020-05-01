78c87355cdc1f0f8f1e15781ca793f19884db896
Aaron Saunders <aaronksaunders@gmail.com>
2020-04-30 20:48:41 -0400

    test on ios device

--- src/components/CameraPage.vue
+++ src/components/CameraPage.vue
@@ -15,13 +15,8 @@
 </template>
 
 <script>
-import {
-  Plugins,
-  CameraSource,
-  CameraResultType,
-} from "@capacitor/core";
+import { Plugins, CameraSource, CameraResultType } from "@capacitor/core";
 const { Camera } = Plugins;
-
 
 export default {
   name: "CameraPage",
@@ -33,8 +28,9 @@ export default {
   },
   methods: {
     scan() {
-       window.cordova.plugins.barcodeScanner.scan(
-        function(result) {
+      window.cordova.plugins.barcodeScanner.scan(
+        result => {
+          console.log(result);
           alert(
             "We got a barcode\n" +
               "Result: " +
@@ -47,7 +43,7 @@ export default {
               result.cancelled
           );
         },
-        function(error) {
+        error => {
           alert("Scanning failed: " + error);
         },
         {
@@ -57,10 +53,8 @@ export default {
           torchOn: true, // Android, launch with the torch switched on (if available)
           saveHistory: true, // Android, save scan history (default false)
           prompt: "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations: true, // iOS
+          disableAnimations: false, // iOS
           disableSuccessBeep: false // iOS and Android
         }
       );
